23179e2bf07a4622fa1114172228bc40707ce362
ren <wjhmkrenren@icloud.com>
2018-02-16 07:16:17 +0000

    twenty_two

--- Gemfile
+++ Gemfile
@@ -25,7 +25,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
-
+gem 'devise'
+gem "paperclip", "~> 5.2.1"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,51 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @posts = Post.all.order("created_at DESC")
+    end
+    
+    def show
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        
+        if @post.save
+            redirect_to @post
+        else
+            render "new"
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render "edit"
+        end
+    end
+    
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
+    
+    private
+    
+    def find_post
+        #自分のコントローラー内のものを見つける時はparams[:id]、自分のコントローラーに無い時(ex:userコントローラー)はparams[:user_id]で参照
+        @post = Post.find(params[:id])
+    end
+    
+    def post_params
+        params.require(:post).permit(:title,:link,:description)
+    end
 end
